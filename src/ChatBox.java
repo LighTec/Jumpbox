ac8a2a268cd9c3a480b60bf3714bcc73b02f32b7
Christilyn A <christilynarjona@outlook.com>
2019-11-07 14:20:55 -0700

    setup controller, populated classes

--- src/ChatBox.java
+++ src/ChatBox.java
@@ -5,12 +5,15 @@ public class ChatBox {
     private ArrayList<Message> messages;
 
 
-
     public void clearMessages() {
         // TOOD
     }
 
     public void addMessage(Message message) {
         // TODO
+    }
+
+    public ArrayList<Message> getMessages() {
+        return messages;
     }
 }
--- src/GameController.java
+++ src/GameController.java
@@ -1,3 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameController {
 
+    private ChatBox chatBox;
+    private Canvas canvas;
+    private ArrayList<Player> players;
+    private TCPClient tcpClient;
+
+    private int timeRemaining;
+    private int round;
+    private String[] wordOptions;
+
+    public GameController(ArrayList<Player> players, TCPClient tcpClient) {
+        this.chatBox = new ChatBox();
+        this.canvas = new Canvas();
+        this.players = players;
+        this.tcpClient = tcpClient;
+        this.round = 0;
+    }
+
+    public void newRound() {
+        this.round = 0;
+        resetCanvas();
+
+        for (Player p : players) p.setDrawer(false);
+    }
+
+    public void setDrawer(Player player) {
+        for (Player p : players) {
+            if (p.getMacAddr().equals(player.getMacAddr())) {
+                p.setDrawer(true);
+            }
+        }
+    }
+
+
+
+    private void resetCanvas() {
+        // TODO
+    }
+
+    private void sendChosenWord() {
+        // TODO send chosen word to tcp client
+    }
+
+    private void sendMessage() {
+        // TODO send guess to tcp client
+    }
+
+
+
+
+
+    public void setTimeRemaining(int timeRemaining) {
+        this.timeRemaining = timeRemaining;
+    }
+
+    public void setWordOptions(String[] wordOptions) {
+        this.wordOptions = wordOptions;
+        // TODO update gui
+    }
+
+    public void sendAllChat(List<Message> messages) {
+        this.chatBox.clearMessages();
+        for (Message m : messages) this.chatBox.addMessage(m);
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public ArrayList<Message> getAllChat() {
+        return chatBox.getMessages();
+    }
 }
